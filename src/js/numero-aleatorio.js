8d0da43f0d902a237387817bcd5fc59890f02e66
matheus-or <matheusolivvee@gmail.com>
2022-05-03 18:38:47 -0300

    Retirada de limite de número.

--- src/js/numero-aleatorio.js
+++ src/js/numero-aleatorio.js
@@ -3,22 +3,18 @@ function mostrarResultado() {
     let numerosSorteados = [];
     let qtdNumeros = document.getElementById("qtdNumeros").value;
 
-    if (qtdNumeros > 6) {
-        alert("No máximo 6 números");
-    } else {
-        while (numerosSorteados.length < qtdNumeros) {
-            let sorteado = Math.floor(Math.random() * (60 - 1) + 1);
-            numerosSorteados.push(sorteado);
-            numerosSorteados = [...new Set(numerosSorteados)];
-        }
+    while (numerosSorteados.length < qtdNumeros) {
+        let sorteado = Math.floor(Math.random() * (60 - 1) + 1);
+        numerosSorteados.push(sorteado);
+        numerosSorteados = [...new Set(numerosSorteados)];
+    }
 
-        if (qtdNumeros.length === 0) {
-            alert('Digite a quantidade de números a ser sorteada!');
-        } else {
-            numerosSorteados.sort(function (a, b) {
-                return a - b
-            });
-            document.getElementById('resultado').innerHTML = 'Números sorteados: ' + numerosSorteados;
-        }
+    if (qtdNumeros.length === 0) {
+        alert('Digite a quantidade de números a ser sorteada!');
+    } else {
+        numerosSorteados.sort(function (a, b) {
+            return a - b
+        });
+        document.getElementById('resultado').innerHTML = 'Números sorteados: ' + numerosSorteados;
     }
 }
